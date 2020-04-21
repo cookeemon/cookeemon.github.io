6ca8cca92c71bb3bb0808a256067481b92f47db6
cookeemon <emonmiahprof@gmail.com>
2020-04-21 18:42:07 -0400

    Updates

---
+++ precache-manifest.db006121e8007a873f0f6a0e341c981b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe60370c80cdcc710e61a9bd1bfcc405",
+    "url": "/cookeemon.github.io/index.html"
+  },
+  {
+    "revision": "56a65aee60d09b6076ae",
+    "url": "/cookeemon.github.io/static/css/main.e66d53e8.chunk.css"
+  },
+  {
+    "revision": "3321656c91d628ad6893",
+    "url": "/cookeemon.github.io/static/js/2.25afbb8e.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/cookeemon.github.io/static/js/2.25afbb8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56a65aee60d09b6076ae",
+    "url": "/cookeemon.github.io/static/js/main.a4447746.chunk.js"
+  },
+  {
+    "revision": "f05e36eac745461e7971",
+    "url": "/cookeemon.github.io/static/js/runtime-main.a0d853f5.js"
+  },
+  {
+    "revision": "5434fdf655c16b9fc3fb742e97620b3f",
+    "url": "/cookeemon.github.io/static/media/ColorChange.5434fdf6.gif"
+  },
+  {
+    "revision": "bfe77692e4d6f1403a4f92af11d1f5c3",
+    "url": "/cookeemon.github.io/static/media/EditButton.bfe77692.gif"
+  },
+  {
+    "revision": "fc96ff24a727634ee3804428ca370e6b",
+    "url": "/cookeemon.github.io/static/media/Kaher.fc96ff24.jpg"
+  }
+]);

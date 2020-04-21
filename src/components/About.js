8436641cc7f2f3adbf272154c835f72ac6574d79
cookeemon <emonmiahprof@gmail.com>
2020-04-21 03:57:56 -0400

    Finished css for about me page

--- src/components/About.js
+++ src/components/About.js
@@ -1,20 +1,25 @@
 import React from 'react';
+import Arrowdown from '../Arrowdown.png';
 
 class About extends React.Component {
    
     render(){
         return(
-            <div className= "about">
-            <div>
-                <h1>About Me</h1>
-                <p>Hello! Welcome to my website. My name is Kaher Miah. I am aspiring to become a
-                    professonal programmer. Problem solving and learning are two things I am passionate
-                    about. After all, it's fun!
-                </p>
-                <ul className="resume-link">
-                    <a href="https://drive.google.com/file/d/1CqL6GSZ1Wut9u0VI3EEmQOTmOHRt8vXk/view?usp=sharing" target="_blank" >Resume</a>
+            <div className= "aboutPage">
+                    <p className="aboutPage2">Hello! Welcome to my website. My name is Kaher Miah. Currently,<br></br>
+                        I live in New York. I go to Lehman College as a full time student.I am aspiring to become a<br></br>
+                        professonal programmer. Problem solving and learning are two things I am passionate about.<br></br>
+                        After all, it's fun!
+                    </p>
+                    <p className="aboutPage3">
+                        Anyways. check out my resume below.
+                    </p>
+                    <img src={Arrowdown} class="arrowImage" alt="Arrow down"/>
+                <ul className="resume-links">
+                    <a href="https://drive.google.com/file/d/1CqL6GSZ1Wut9u0VI3EEmQOTmOHRt8vXk/view?usp=sharing" target="_blank" 
+                    className= "resume-link">Resume</a>
                 </ul>
-            </div>
+            
             </div>
         )
     }
